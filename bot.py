0aec9b96d142590538804c2a2ba6f4b4ece8406e
kimmzione <164731177+kimmzione@users.noreply.github.com>
2024-05-01 13:19:49 +0000

    저장

--- bot.py
+++ bot.py
@@ -21,6 +21,20 @@ while True:
         break
     else:
         response = random.choices(random_responses)[0]
+        # Translate English responses into Korean
+        if response == "That is quite interesting, please tell me more.":
+            response = "매우 흥미로워요. 더 말해주세요."
+        elif response == "I see. Do go on.":
+            response = "알겠어요. 계속 이야기해 주세요."
+        elif response == "Why do you say that?":
+            response = "왜 그러시나요?"
+        elif response == "Funny weather we've been having, isn't it?":
+            response = "요즘 날씨가 참 재미있네요, 안 그래요?"
+        elif response == "Let's change the subject.":
+            response = "주제를 바꿔볼까요?"
+        elif response == "Did you catch the game last night?":
+            response = "어젯밤 경기 보셨어요?"
     print(response)
 
 print("It was nice talking to you, goodbye!")
+
